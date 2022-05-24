2ebd05b5013912cd6e29d9655ea9380f8502eb7a
frann-santtos <francisco.santos@dcx.ufpb.br>
2022-05-24 16:16:13 -0300

    refs #004
    adicionando a classe Francisco.

---
+++ atv-git/src/main/java/Francisco.java
@@ -0,0 +1,16 @@
+public class Francisco {
+
+    Integer a;
+    Integer b;
+    Integer result;
+
+    public Francisco (Integer a, Integer a){
+        this.a = a;
+        this.b = b;
+        this.result = sum(a, b);
+    }
+
+    private static Integer som(Integer n1, Integer n2){
+        return n1 + n2;
+    }
+}
--- atv-git/src/main/java/Main.java
+++ atv-git/src/main/java/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main (String[] args){
         Drayon drayon = new Drayon(8,8);
         System.out.println(drayon.resultado);
+
+
+        Francisco francisco = new Francisco(24,858);
+        System.out.println(francisco.result);
     }
 }
